c3559d85b6243c020766ea61894bbd306003a3b9
Bing Xu <bingxu@fb.com>
2022-03-26 11:32:43 -0700

    asdf

--- examples/xx_bmm_universal_bias_relu/bmm_bias_relu.cu
+++ examples/xx_bmm_universal_bias_relu/bmm_bias_relu.cu
@@ -1,7 +1,6 @@
 
 #include <iostream>
 #include "cutlass/cutlass.h"
-#include "cutlass/epilogue/thread/linear_combination_relu.h"
 #include "cutlass/gemm/device/gemm_universal.h"
 #include "cutlass/util/host_tensor.h"
 #include "cutlass/util/reference/host/tensor_fill.h"
@@ -16,11 +15,11 @@
     }                                                                     \
   }
 
-// Gemm operator cutlass_tensorop_f16_s16816gemm_f16_128x128_32x3_tn_align8
-using Operation_cutlass_tensorop_f16_s16816gemm_f16_128x128_32x3_tn_align8 =
+// Gemm operator cutlass_tensorop_f16_s16816gemm_f16_128x64_32x6_nn_align2
+using Operation_cutlass_tensorop_f16_s16816gemm_f16_128x64_32x6_nn_align2 =
     cutlass::gemm::device::GemmUniversal<
         cutlass::half_t,
-        cutlass::layout::RowMajor,
+        cutlass::layout::ColumnMajor,
         cutlass::half_t,
         cutlass::layout::ColumnMajor,
         cutlass::half_t,
@@ -28,27 +27,26 @@ using Operation_cutlass_tensorop_f16_s16816gemm_f16_128x128_32x3_tn_align8 =
         float,
         cutlass::arch::OpClassTensorOp,
         cutlass::arch::Sm80,
-        cutlass::gemm::GemmShape<128, 128, 32>,
-        cutlass::gemm::GemmShape<64, 64, 32>,
+        cutlass::gemm::GemmShape<128, 64, 32>,
+        cutlass::gemm::GemmShape<64, 32, 32>,
         cutlass::gemm::GemmShape<16, 8, 16>,
 
         cutlass::epilogue::thread::
-            LinearCombinationReLU<cutlass::half_t, 8, float, float>,
+            LinearCombination<cutlass::half_t, 2, float, float>,
         cutlass::gemm::threadblock::GemmIdentityThreadblockSwizzle<8>,
-        3,
-        8,
-        8,
+        6,
+        2,
+        2,
 
         cutlass::arch::OpMultiplyAdd
 
         >;
 
 using BMMInstance =
-    Operation_cutlass_tensorop_f16_s16816gemm_f16_128x128_32x3_tn_align8;
+    Operation_cutlass_tensorop_f16_s16816gemm_f16_128x64_32x6_nn_align2;
 
 void bmm(cutlass::half_t* a_ptr,
          cutlass::half_t* b_ptr,
-         cutlass::half_t* bias_ptr,
          cutlass::half_t* c_ptr,
          int* a_dim0,
          int* a_dim1,
@@ -60,60 +58,63 @@ void bmm(cutlass::half_t* a_ptr,
          int* c_dim1,
          int* c_dim2) {
   int AB = *a_dim0;
-  int AM = *a_dim1;
-  int AK = *a_dim2;
+  int AK = *a_dim1;
+  int AM = *a_dim2;
   int BB = *b_dim0;
   int BN = *b_dim1;
   int BK = *b_dim2;
-  int CB = BB;
+  int CB = AB;
   int CM = AM;
   int CN = BN;
   *c_dim0 = CB;
   *c_dim1 = CM;
   *c_dim2 = CN;
-  const int B = BB;
+  const int B = AB;
   const int M = AM;
   const int N = BN;
   const int K = AK;
 
-  using ElementComputeEpilogue = typename BMMInstance::ElementAccumulator;
+  if (B == 1024 && M == 128 && N == 30 && K == 752) {
+    //  TODO: cast to right dtype
+    using ElementComputeEpilogue = typename BMMInstance::ElementAccumulator;
 
-  typename BMMInstance::Arguments arguments{
-      cutlass::gemm::GemmUniversalMode::kBatched,
-      {AM, BN, AK},
-      AB,
-      {ElementComputeEpilogue(1), ElementComputeEpilogue(1)},
-      (void*)a_ptr,
-      (void*)b_ptr,
-      (void*)bias_ptr,
-      (void*)c_ptr,
-      0,
-      BN * BK,
-      CN,
-      CM * CN,
-      AK,
-      BK,
-      CN,
-      CN};
+    typename BMMInstance::Arguments arguments{
 
-  BMMInstance gemm_op;
-  size_t workspace_size = gemm_op.get_workspace_size(arguments);
-  // TODO: handle workspace correctly
-  // This is a shitty code happened to be ok for non-split-k case
-  // Because for fprop the workspace is 0
-  cutlass::device_memory::allocation<uint8_t> workspace(workspace_size);
-  auto status = gemm_op.can_implement(arguments);
-  CUTLASS_CHECK(status);
-  status = gemm_op.initialize(arguments, workspace.get());
-  CUTLASS_CHECK(status);
-  status = gemm_op();
-  CUTLASS_CHECK(status);
-  return;
+        cutlass::gemm::GemmUniversalMode::kBatched,
+        {AM, BN, AK},
+        AB,
+        {ElementComputeEpilogue(1), ElementComputeEpilogue(0)},
+        (void*)a_ptr,
+        (void*)b_ptr,
+        (void*)c_ptr,
+        (void*)c_ptr,
+        AK * AM,
+        0,
+        CM * CN,
+        CM * CN,
+        AM,
+        BK,
+        CN,
+        CN
+
+    };
+    BMMInstance gemm_op;
+    size_t workspace_size = gemm_op.get_workspace_size(arguments);
+    // TODO: handle workspace correctly
+    // This is a shitty code happened to be ok for non-split-k case
+    // Because for fprop the workspace is 0
+    cutlass::device_memory::allocation<uint8_t> workspace(workspace_size);
+    auto status = gemm_op.can_implement(arguments);
+    CUTLASS_CHECK(status);
+    status = gemm_op.initialize(arguments, workspace.get());
+    CUTLASS_CHECK(status);
+    status = gemm_op();
+    CUTLASS_CHECK(status);
+    return;
+  }
+  throw std::runtime_error(
+      "Unsupported workload for this conv2d specialization.");
 }
-throw std::runtime_error(
-    "Unsupported workload for this conv2d specialization.");
-}
-
 int main(int argc, char** argv) {
   int B = std::atoi(argv[1]);
   int M = std::atoi(argv[2]);
@@ -125,10 +126,10 @@ int main(int argc, char** argv) {
   // Bias is [B, N]
   // Y is [B, M, N]
 
-  int a_dim0 = 1;
-  int a_dim1 = M;
-  int a_dim2 = K;
-  int b_dim0 = B;
+  int a_dim0 = B;
+  int a_dim1 = K;
+  int a_dim2 = M;
+  int b_dim0 = 1;
   int b_dim1 = N;
   int b_dim2 = K;
   int c_dim0 = B;
@@ -141,7 +142,7 @@ int main(int argc, char** argv) {
   int BB = b_dim0;
   int BN = b_dim1;
   int BK = a_dim2;
-  int CB = BB;
+  int CB = AB;
   int CM = AM;
   int CN = BN;
 
@@ -156,12 +157,12 @@ int main(int argc, char** argv) {
   cutlass::HostTensor<ElementOutput, typename GemmInstance::LayoutC> c(
       {c_dim0 * c_dim1, c_dim2});
   cutlass::HostTensor<ElementInputA, typename GemmInstance::LayoutA> bias(
-      {c_dim0, c_dim2});
+      {c_dim2});
 
   // warmup
 
   bmm((cutlass::half_t*)a.device_data(), (cutlass::half_t*)b.device_data(),
-      (cutlass::half_t*)bias.device_data()(cutlass::half_t*) c.device_data(),
+      (cutlass::half_t*)bias.device_data(), (cutlass::half_t*)c.device_data(),
       &a_dim0, &a_dim1, &a_dim2, &b_dim0, &b_dim1, &b_dim2, &c_dim0, &c_dim1,
       &c_dim2);
   cudaEvent_t events[2];
@@ -171,8 +172,9 @@ int main(int argc, char** argv) {
   cudaEventRecord(events[0]);
   for (int i = 0; i < 5; ++i) {
     bmm((cutlass::half_t*)a.device_data(), (cutlass::half_t*)b.device_data(),
-        (cutlass::half_t*)c.device_data(), &a_dim0, &a_dim1, &a_dim2, &b_dim0,
-        &b_dim1, &b_dim2, &c_dim0, &c_dim1, &c_dim2);
+        (cutlass::half_t*)bias.device_data(), (cutlass::half_t*)c.device_data(),
+        &a_dim0, &a_dim1, &a_dim2, &b_dim0, &b_dim1, &b_dim2, &c_dim0, &c_dim1,
+        &c_dim2);
   }
   cudaEventRecord(events[1]);
   cudaEventSynchronize(events[1]);
